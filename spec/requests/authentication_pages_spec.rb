0fc277827e1ec1b39ed75066444a5780ca1cb59b
Eric So <services@ericso.me>
2012-12-15 12:07:25 -0800

    Finish user edit, update, index, and destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -24,7 +24,7 @@ describe "Authentication" do
 	    before { click_link "Home" }
 	    it { should_not have_selector('div.alert.alert-error') }
 	  end
-    end
+  end
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
@@ -48,7 +48,7 @@ describe "Authentication" do
       	it { should have_link "Sign in" }
       end
 	end
-  end
+end
 
   # Is the user a known user
   describe "authorization" do
@@ -69,7 +69,7 @@ describe "Authentication" do
   	      	page.should have_selector('title', text: 'Edit user')
   	      end
   	    end
-	  end
+	    end
 
   	  describe "in the Users controller" do
   	  	# non-signed in and trying to edit user
@@ -98,16 +98,27 @@ describe "Authentication" do
   	  let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
   	  before { sign_in user }
 
-	  describe "visiting Users#edit page" do
-	  	before { visit edit_user_path(wrong_user) }
-	  	it { should_not have_selector('title', text: full_title('Edit user')) }
+	    describe "visiting Users#edit page" do
+	  	  before { visit edit_user_path(wrong_user) }
+	    	it { should_not have_selector('title', text: full_title('Edit user')) }
+	    end
+
+	    describe "submitting a PUT request to the Users#update action" do
+	    	before { put user_path(wrong_user) }
+	    	specify { response.should redirect_to(root_path) }
+	    end
 	  end
 
-	  describe "submitting a PUT request to the Users#update action" do
-	  	before { put user_path(wrong_user) }
-	  	specify { response.should redirect_to(root_path) }
-	  end
-	end
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { response.should redirect_to(root_path) }
+      end
+    end
   end
-
 end
